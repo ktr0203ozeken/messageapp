55d9bcef32668b62b185b598a54faacbdf3279a0
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-04-22 16:50:29 +0900

    sqlファイルのテーブル名の変更、カラムの追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 --messageテーブルへのデータ挿入
-INSERT INTO message (content) VALUES ('こんにちは、世界!');
-INSERT INTO message (content) VALUES ('Hello,World!');
-INSERT INTO message (content) VALUES ('你好、世界!');
+INSERT INTO messages (content) VALUES ('こんにちは、世界!');
+INSERT INTO messages (content) VALUES ('Hello,World!');
+INSERT INTO messages (content) VALUES ('你好、世界!');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE message(
+CREATE TABLE messages (
 	message_id BIGINT PRIMARY KEY AUTO_INCREMENT,
 	content VARCHAR(255) NOT NULL,
-	created_at TIMESTAMP 
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP
 );
