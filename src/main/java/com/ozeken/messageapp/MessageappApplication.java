ee5446f5b6eb2a996f794a08a98fa091354d96de
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-04-22 17:05:22 +0900

    -バリデーションの依存関係を追加 -アプリケーションクラス名をMessageAppApplicationに変更

--- src/main/java/com/ozeken/messageapp/MessageappApplication.java
+++ src/main/java/com/ozeken/messageapp/MessageappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessageappApplication {
+public class MessageAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessageappApplication.class, args);
+		SpringApplication.run(MessageAppApplication.class, args);
 	}
-
+	
 }
